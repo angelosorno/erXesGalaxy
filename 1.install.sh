fdaaa92ddf59058c8becd0c7f1e02b6aecb8dea8
angelosorno <angel@moratoo.com>
2024-08-01 16:07:58 -0500

    🚧 Create Install.sh

--- 1.install.sh
+++ 1.install.sh
@@ -1,71 +1,78 @@
 #!/bin/bash
 
-# Registrar la hora actual
-now=$(date +'%H:%M:%S')
+# Función para registrar la hora actual
+log_time() {
+    echo $(date +'%H:%M:%S')
+}
+
+# Función para imprimir mensajes con formato
+print_step() {
+    echo -e "$2 Step $(log_time) : $1"
+}
 
 # Paso 1: Actualizar el sistema
-echo -e "🚀 Step 1 $now : Updating system"
-sudo yum update -y
-sudo yum install epel-release -y
+print_step "Updating system" "🚀"
+sudo apt-get update -y
 
 # Paso 2: Instalar nginx
-now=$(date +'%H:%M:%S')
-echo -e "🌐 Step 2 $now : Installing nginx"
-sudo yum install nginx -y
-sudo systemctl start nginx
-sudo yum check-update
+print_step "Installing nginx" "🌐"
+sudo apt install nginx -y
+sudo apt update -y
 
-# Paso 3: Instalar Docker
-now=$(date +'%H:%M:%S')
-echo -e "🐳 Step 3 $now : Installing Docker"
-curl -fsSL https://get.docker.com/ | sh
-sudo systemctl start docker
-sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+# Paso 3: Instalar Docker si no está instalado
+if ! [ -x "$(command -v docker)" ]; then
+    print_step "Installing Docker" "🐳"
+    sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+    apt-cache policy docker-ce
+    sudo apt install docker-ce -y
+else
+    print_step "Docker is already installed" "🐳"
+fi
 
-# Paso 4: Instalar docker-compose
-now=$(date +'%H:%M:%S')
-echo -e "🔧 Step 4 $now : Installing docker-compose"
-sudo yum install docker-ce -y
-sudo yum install http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/p/pigz-2.3.4-1.el7.x86_64.rpm -y
-sudo yum install http://mirror.centos.org/centos/7/extras/x86_64/Packages/container-selinux-2.21-1.el7.noarch.rpm -y
-sudo yum list docker-ce --showduplicates | sort -r docker-ce.x86_64  17.09.ce-1.el7.centos  docker-ce-stable
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
+# Paso 4: Instalar docker-compose si no está instalado
+if ! [ -x "$(command -v docker-compose)" ]; then
+    print_step "Installing docker-compose" "🔧"
+    sudo apt-get update -y
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+else
+    print_step "docker-compose is already installed" "🔧"
+fi
 
 # Paso 5: Instalar Node.js
-now=$(date +'%H:%M:%S')
-echo -e "🟢 Step 5 $now : Installing Node.js"
-curl -sL https://rpm.nodesource.com/setup_20.x | sudo bash -
-sudo yum install nodejs -y
-node --version
+if ! [ -x "$(command -v node)" ]; then
+    print_step "Installing Node.js" "🟢"
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+    curl -s https://deb.nodesource.com/setup_18.x | sudo bash
+    sudo apt-get install nodejs -y
+else
+    print_step "Node.js is already installed" "🟢"
+fi
 
 # Paso 6: Instalar npm
-now=$(date +'%H:%M:%S')
-echo -e "📦 Step 6 $now : Installing npm"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm --version
-nvm install node
-nvm install --lts
-nvm install 9.6.7
+if ! [ -x "$(command -v npm)" ]; then
+    print_step "Installing npm" "📦"
+    sudo apt install npm -y
+else
+    print_step "npm is already installed" "📦"
+fi
 
 # Paso 7: Instalar AWS CLI
-now=$(date +'%H:%M:%S')
-echo -e "☁️ Step 7 $now : Installing AWS CLI"
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-aws --version
+if ! [ -x "$(command -v aws)" ]; then
+    print_step "Installing AWS CLI" "☁️"
+    sudo apt-get install awscli -y
+else
+    print_step "AWS CLI is already installed" "☁️"
+fi
 
 # Paso 8: Instalar create-erxes-app
-now=$(date +'%H:%M:%S')
-echo -e "✨ Step 8 $now : Installing create-erxes-app"
-sudo npm install -g create-erxes-app -y
+if ! [ -x "$(command -v create-erxes-app)" ]; then
+    print_step "Installing create-erxes-app" "✨"
+    sudo npm install -g create-erxes-app -y
+else
+    print_step "create-erxes-app is already installed" "✨"
+fi
 
-# Paso 9: Instalar jq
-now=$(date +'%H:%M:%S')
-echo -e "🔧 Step 9 $now : Installing jq"
-sudo yum install -y jq
-
-echo -e "🎉 All steps completed successfully!"
+print_step "All steps completed successfully!" "🎉"
